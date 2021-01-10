51ed8554ea4e26d10dd85f3c96332ed26f076f28
hraimk <kaouthar.hraim@grenoble-inp.org>
2021-01-10 16:54:05 +0100

    début gestion données

--- include/IMarketData.h
+++ include/IMarketData.h
@@ -1,4 +1,17 @@
 #pragma once
+#include <string>
+#include <map>
+#include "DataTime.h"
+#include "DataFeed.h"
+using namespace std;
 
 class IMarketData{
+    public:
+    DateTime * listeDate_;
+
+    IMarketData(DateTime * listeDate);
+
+    virtual ~IMarketData();
+    virtual DataFeed * getMarketdata();
+
 };
