6af785ce72a1fdc304ffa58f5138f437a301fdec
joel zhu <joel.zhu@greonble-inp.org>
2021-01-12 00:47:43 +0100

    reverted

--- src/BlackScholesModel.cpp
+++ src/BlackScholesModel.cpp
@@ -15,22 +15,21 @@ BlackScholesModel::~BlackScholesModel()
 
 void BlackScholesModel::asset(PnlRng *rng)
 {
+    // TODO: works for derivative of size 1, make this work with derivative with d underlyings (instead of [0] everywhere)
     double timestep = this->derivative_->T_/this->derivative_->nbTimeSteps_;
     PnlVect *row = pnl_vect_create(this->derivative_->size_);
-    for(int d = 0; d < this->derivative_->size_/2; ++d){
-        for (int k = 1; k <= this->derivative_->nbTimeSteps_; ++k)
-        {
-            pnl_vect_rng_normal(this->G_, this->derivative_->size_, rng); // G Vecteur gaussien
-            pnl_mat_mult_vect_inplace(this->B_, this->sigma_, this->G_);
+    for (int k = 1; k <= this->derivative_->nbTimeSteps_; ++k)
+    {
+        pnl_vect_rng_normal(this->G_, this->derivative_->size_, rng); // G Vecteur gaussien
+        pnl_mat_mult_vect_inplace(this->B_, this->sigma_, this->G_);
 
-            pnl_mat_get_row(row, this->sigma_, d*2);
-            double sigma_d = pnl_vect_norm_two(row);
-            LET(this->derivative_->underlyings_[d]->zc_, k) = GET(this->derivative_->underlyings_[d]->zc_, k-1) * exp( (this->derivative_->rd_ - (sigma_d*sigma_d)/2 ) * timestep + sqrt(timestep) * GET(this->B_, d*2));
+        pnl_mat_get_row(row, this->sigma_, 0);
+        double sigma_d = pnl_vect_norm_two(row);
+        LET(this->derivative_->underlyings_[0]->zc_, k) = GET(this->derivative_->underlyings_[0]->zc_, k-1) * exp( (this->derivative_->rd_ - (sigma_d*sigma_d)/2 ) * timestep + sqrt(timestep) * GET(this->B_, 0));
 
-            pnl_mat_get_row(row, this->sigma_, (d*2)+1);
-            sigma_d = pnl_vect_norm_two(row);
-            LET(this->derivative_->underlyings_[d]->price_, k) = GET(this->derivative_->underlyings_[d]->price_, k-1) * exp( (this->derivative_->rd_ - (sigma_d*sigma_d)/2 ) * timestep + sqrt(timestep) * GET(this->B_, (d*2)+1));
-        }
+        pnl_mat_get_row(row, this->sigma_, 1);
+        sigma_d = pnl_vect_norm_two(row);
+        LET(this->derivative_->underlyings_[0]->price_, k) = GET(this->derivative_->underlyings_[0]->price_, k-1) * exp( (this->derivative_->rd_ - (sigma_d*sigma_d)/2 ) * timestep + sqrt(timestep) * GET(this->B_, 1));
     }
     
     pnl_vect_free(&row);
