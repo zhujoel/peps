7a7b559106bb154df0c5d4a414fd6624c4d9e330
joel zhu <joel.zhu@greonble-inp.org>
2021-01-21 16:01:17 +0100

    tests datetime

--- tests/test_DateTime.cxx
+++ tests/test_DateTime.cxx
@@ -1,23 +1,80 @@
 #include "gtest/gtest.h"
 #include "DateTime.h"
-#include "pnl/pnl_vector.h"
 #include <fstream>
-#include <chrono>
 
 class DateTimeTest: public ::testing::Test{
     protected:
+        DateTime *dt;
+
         virtual void SetUp(){
+            this->dt = new DateTime(15, 7, 1992);
         }
 
         virtual void TearDown(){
         }
 };
 
-TEST_F(DateTimeTest, constructorDefault){
-    DateTime *dt = new DateTime(1, 1, 1);
-    PnlVect *vect = pnl_vect_create(2);
-    EXPECT_EQ(1, dt->dd_);
-    EXPECT_EQ(0, GET(vect, 0));
+TEST_F(DateTimeTest, constructorJour){
+    EXPECT_EQ(15, this->dt->dd_);
+}
+
+TEST_F(DateTimeTest, constructorMonth){
+    EXPECT_EQ(7, this->dt->mm_);
+}
+
+TEST_F(DateTimeTest, constructorYear){
+    EXPECT_EQ(1992, this->dt->yyyy_);
+}
+
+TEST_F(DateTimeTest, equalsDayBefore){
+    DateTime *dt2 = new DateTime(25, 7, 1992);
+    EXPECT_EQ(-1, this->dt->compare(dt2));
+}
+
+TEST_F(DateTimeTest, equalsDaySame){
+    DateTime *dt2 = new DateTime(15, 7, 1992);
+    EXPECT_EQ(0, this->dt->compare(dt2));
+}
+
+TEST_F(DateTimeTest, equalsDayAfter){
+    DateTime *dt2 = new DateTime(10, 7, 1992);
+    EXPECT_EQ(1, this->dt->compare(dt2));
+}
+
+TEST_F(DateTimeTest, equalsMonthBefore){
+    DateTime *dt2 = new DateTime(15, 10, 1992);
+    EXPECT_EQ(-1, this->dt->compare(dt2));
+}
+
+TEST_F(DateTimeTest, equalsMonthSame){
+    DateTime *dt2 = new DateTime(15, 7, 1992);
+    EXPECT_EQ(0, this->dt->compare(dt2));
+}
+
+TEST_F(DateTimeTest, equalsMonthAfter){
+    DateTime *dt2 = new DateTime(15, 3, 1992);
+    EXPECT_EQ(1, this->dt->compare(dt2));
+}
+
+TEST_F(DateTimeTest, equalsYearBefore){
+    DateTime *dt2 = new DateTime(15, 7, 1993);
+    EXPECT_EQ(-1, this->dt->compare(dt2));
+}
+
+TEST_F(DateTimeTest, equalsYearSame){
+    DateTime *dt2 = new DateTime(15, 7, 1992);
+    EXPECT_EQ(0, this->dt->compare(dt2));
+}
+
+TEST_F(DateTimeTest, equalsYearAfter){
+    DateTime *dt2 = new DateTime(15, 7, 1990);
+    EXPECT_EQ(1, this->dt->compare(dt2));
+}
+
+TEST_F(DateTimeTest, operatorPrint){
+    std::stringstream str;
+    str << this->dt;
+    EXPECT_EQ("15/7/1992", str.str());
 }
 
 int main(int argc, char** argv){
