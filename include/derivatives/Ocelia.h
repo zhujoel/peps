3948c39ff9d1dd7954cd7ab239858bac5b585c6c
joel zhu <joel.zhu@greonble-inp.org>
2021-02-25 01:06:27 +0100

    facto oceliaé

--- include/derivatives/Ocelia.h
+++ include/derivatives/Ocelia.h
@@ -2,37 +2,32 @@
 
 #include "derivatives/IDerivative.h"
 #include "libs/DateTimeVector.h"
-#include <string>
 #include "pnl/pnl_matrix.h"
+#include <vector>
 
 class Ocelia : public IDerivative{
     public:
-        // // TODO: data:
-        // // 1 vecteur date avec toutes les dates du début 15 mai 2008 à 28 avril 2016
-        // // 4 underlyings: 4 sous-jacent (euro, gbp, jpy, chf): prix et zc + spot
+        int annee_payoff;
+        int nb_sous_jacents_;
+        PnlVectInt *indices_dates_semestrielles_; // indices des dates de constatation dans le path 
+        PnlVectInt *indices_dates_valeurs_n_ans_; // indices dans le path des dates des valeurs n ans
+        PnlVect *valeurs_n_ans_; // moyenne de la valeur des indices à l'année n
+        PnlVect *valeurs_initiales_; // moyenne des valeurs initiales (année 0);
+        PnlVect *nouveau_depart_; // valeurs de l'effet nouveau départ
+        PnlVect *perfs_; // perfs à l'année n
 
-        // int annee_payoff;
-        // int nb_sous_jacents_;
-        // PnlVectInt *indices_dates_semestrielles_; // indices des dates de constatation dans le path 
-        // PnlVectInt *indices_dates_valeurs_n_ans_; // indices dans le path des dates des valeurs n ans
-        // PnlVect *valeurs_n_ans_; // moyenne de la valeur des indices à l'année n
-        // PnlVect *valeurs_initiales_; // moyenne des valeurs initiales (année 0);
-        // PnlVect *nouveau_depart_; // valeurs de l'effet nouveau départ
-        // PnlVect *perfs_; // perfs à l'année n
+        Ocelia(double T, int nbTimeSteps, int size, int nb_sous_jacents, std::vector<DateTime*> all_dates);
+        ~Ocelia();
 
-        // Ocelia(double T, int nbTimeSteps, int size, int nb_sous_jacents, std::vector<DateTime*> all_dates);
-        // ~Ocelia();
+        void adjust_sigma(PnlMat *sigma); // adjust computed sigma en fonction du nombre de produit dans la matrix path et de comment ils sont organisés
+        void adjust_spot(PnlMat *past);
 
-
-        // void adjust_sigma(PnlMat *sigma); // adjust computed sigma en fonction du nombre de produit dans la matrix path et de comment ils sont organisés
-        // void adjust_spot(PnlVect *spot); // same as above
-
-        // double getMaturity();
-        // double get_foreign_index_market_value(const PnlMat* path, int date_idx, int idx);
-        // double compute_perf_moyenne_panier(const PnlMat *path);
-        // void compute_valeurs_n_ans(const PnlMat *path, PnlVect *valeurs, int N); // calcule la valeur moyenne des indices pour l'année n
-        // void compute_perfs_n_ans(const PnlMat *path, PnlVect *perfs, int N); // compute performance pour un indice à une année
-        // void compute_nouveau_depart(const PnlMat *path); 
-        // double compute_flux_n_ans(const PnlMat *path, int N); // calcul du C(N) cf 1.3
-        // double payoff(const PnlMat *path);
+        double getMaturity();
+        double get_foreign_index_market_value(const PnlMat* path, int date_idx, int idx);
+        double compute_perf_moyenne_panier(const PnlMat *path);
+        void compute_valeurs_n_ans(const PnlMat *path, PnlVect *valeurs, int N); // calcule la valeur moyenne des indices pour l'année n
+        void compute_perfs_n_ans(const PnlMat *path, PnlVect *perfs, int N); // compute performance pour un indice à une année
+        void compute_nouveau_depart(const PnlMat *path); 
+        double compute_flux_n_ans(const PnlMat *path, int N); // calcul du C(N) cf 1.3
+        double payoff(const PnlMat *path);
 };
--- src/derivatives/IDerivative.cpp
+++ src/derivatives/IDerivative.cpp
@@ -1,11 +1,11 @@
-// #include "derivatives/IDerivative.h"
+#include "derivatives/IDerivative.h"
 
-// IDerivative::IDerivative(double T, int nbTimeSteps, int size){
-//     this->T_ = T;
-//     this->nbTimeSteps_ = nbTimeSteps;
-//     this->size_ = size;
-// }
+IDerivative::IDerivative(double T, int nbTimeSteps, int size){
+    this->T_ = T;
+    this->nbTimeSteps_ = nbTimeSteps;
+    this->size_ = size;
+}
 
-// IDerivative::~IDerivative(){
+IDerivative::~IDerivative(){
     
-// }
+}
