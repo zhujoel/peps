763ee1d7e750c7408803494f8a0dd3f24a78406f
joel zhu <joel.zhu@greonble-inp.org>
2021-03-04 17:56:35 +0100

    quanto debugged

--- include/derivatives/QuantoOption.h
+++ include/derivatives/QuantoOption.h
@@ -7,14 +7,15 @@
 class QuantoOption : public IDerivative {
 
   public:
-    // double K_; // strike en devise domestique de l'option Quanto
-    // double rf_; // taux d'intéret étranger (foreign rate) -- on considère que c'est un taux constant (TODO: à changer si nécessaire)
+    double K_; // strike en devise domestique de l'option Quanto
+    double rf_; // taux d'intéret étranger (foreign rate) -- on considère que c'est un taux constant (TODO: à changer si nécessaire)
 
-    //  QuantoOption(double T, int nbTimeSteps, int size, double rf, double K);
-    // ~QuantoOption();
+    QuantoOption(double T, int nbTimeSteps, int size, double rf, double K);
+    ~QuantoOption();
     
-    // double getMaturity();
-    // void adjust_sigma(PnlMat *sigma); // adjust computed sigma en fonction du nombre de produit dans la matrix path et de comment ils sont organisés
-    // void adjust_spot(PnlVect *spot); // same as above
-    // double payoff(const PnlMat *path);
+    double get_annee_payoff();
+    void adjust_sigma(PnlMat *sigma); // adjust computed sigma en fonction du nombre de produit dans la matrix path et de comment ils sont organisés
+    void adjust_past(PnlMat *past);
+    void adjust_spot(PnlVect *spot);
+    double payoff(const PnlMat *path);
 };
--- src/derivatives/QuantoOption.cpp
+++ src/derivatives/QuantoOption.cpp
@@ -1,30 +1,30 @@
-// #include "derivatives/QuantoOption.h"
-// #include "pnl/pnl_mathtools.h"
+#include "derivatives/QuantoOption.h"
+#include "pnl/pnl_mathtools.h"
 
-// QuantoOption::QuantoOption(double T, int nbTimeSteps, int size, double rf, double K) : IDerivative(T, nbTimeSteps, size){
-//     this->rf_ = rf;
-//     this->K_ = K;
-// }
+QuantoOption::QuantoOption(double T, int nbTimeSteps, int size, double rf, double K) : IDerivative(T, nbTimeSteps, size){
+    this->rf_ = rf;
+    this->K_ = K;
+}
 
-// QuantoOption::~QuantoOption(){
+QuantoOption::~QuantoOption(){
 
-// }
+}
 
-// double QuantoOption::getMaturity(){
-//     return this->T_;
-// }
+double QuantoOption::get_annee_payoff(){
+    return this->T_;
+}
 
-// void QuantoOption::adjust_sigma(PnlMat *sigma){
-//     // pas besoin d'ajuster pour un quanto
-// }
+void QuantoOption::adjust_sigma(PnlMat *sigma){ // pas besoin d'ajuster dans quanto
+}
+void QuantoOption::adjust_past(PnlMat *past){ 
+}
+void QuantoOption::adjust_spot(PnlVect *spot){
+}
 
-// void QuantoOption::adjust_spot(PnlVect *spot){
-//     // pas besoin d'ajuster pour un quanto
-// }
 
-// double QuantoOption::payoff(const PnlMat *path){
-//     // double B_T = MGET(path, this->nbTimeSteps_, 0); // valeur finale de l'actif sans risque converti 
-//     double S_T = MGET(path, this->nbTimeSteps_, 1); // valeur finale de l'actif risqué converti
+double QuantoOption::payoff(const PnlMat *path){
+    // double B_T = MGET(path, this->nbTimeSteps_, 0); // valeur finale de l'actif sans risque converti 
+    double S_T = MGET(path, this->nbTimeSteps_, 1); // valeur finale de l'actif risqué converti
 
-//     return MAX(S_T - this->K_, 0);   
-// }
+    return MAX(S_T - this->K_, 0);   
+}
